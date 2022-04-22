471ddafdd178c401a8bd51594393615bef962539
Alex-Kiosa <akiosa88@gmail.com>
2022-04-22 23:27:31 +0400

    Wrote function isNarcissistic() and edited index.html

--- script.js
+++ script.js
@@ -1,41 +1,52 @@
 "use strict"
 
-// RGB to HEX conversion
-function inRange(x) {
-	if ((x - 0) * (x - 255) <= 0) {
-		return x;
-	} else if (x < 0) {
-		return 0;
-	} else {
-		return 255;
+function isNarcissistic(n){
+	let str = n.toString();
+	let result = 0;
+	for (let i = 0; i < str.length; i++) {
+		result += Math.pow(str[i], str.length);
 	}
+ 	return result === n;
 }
 
-function rgb(r, g, b) {
-	let itemsBase16 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-	let arr1 = [], arr2 = [], arr3 = [], 
-		q1 = inRange(r),
-		q2 = inRange(g),
-		q3 = inRange(b);
+console.log(isNarcissistic(370), true);
+console.log(isNarcissistic(324), false);
+console.log(isNarcissistic(371), true);
+console.log(isNarcissistic(4328), false);
+console.log(isNarcissistic(407), true);
+console.log(isNarcissistic(3248), false);
+console.log(isNarcissistic(54748), true);
+console.log(isNarcissistic(92727), true);
+console.log(isNarcissistic(93084), true);
 
-	for (let i = 2; i > 0; i--) {
-		arr1.unshift(itemsBase16[q1 % 16]);
-		q1 = Math.floor(q1 / 16);
+/*function isValidIP(str) {
+	let regexp = /[1-2]{0,}[0-5]{0,}[0-5]/;
+	
+	return regexp.test(str);
+}*/
+
+/*function isValidIP(str) {
+	let octet = str.split(".");
+	let result;
+
+	for (let i = 0; i < octet.length; i++) {
+		let isValidNumber = (octet[i] >= 0 && octet[i] <= 255);
+		if (!isValidNumber || octet[i] == '' || /^0\d/.test(octet[i]) || !octet.length == 4) {
+			result = false;
+			break;
+		} else {
+			result = true;
+		}
 	}
 
-	for (let i = 2; i > 0; i--) {
-		arr2.unshift(itemsBase16[q2 % 16]);
-		q2 = Math.floor(q2 / 16);
-	}
+	return result;
+}*/
 
-	for (let i = 2; i > 0; i--) {
-		arr3.unshift(itemsBase16[q3 % 16]);
-		q3 = Math.floor(q3 / 16);
-	}
-
-	return arr1.concat(arr2, arr3).join('');
-}
-
-rgb(0, 0, -20) // returns 000000
-rgb(300,255,255) // returns FFFFFF
-rgb(173,255,47) // returns ADFF2Fs
+/*console.log(isValidIP("0.0.0.0"), true); // true
+console.log(isValidIP("02.03.04.0"), false); // false
+console.log(isValidIP("12.255.56.1"),true); // 
+console.log(isValidIP("137.255.156.100"), true); // true
+console.log(isValidIP(''), false); // false
+console.log(isValidIP('abc.def.ghi.jkl'), false); // false
+console.log(isValidIP('123.456.789.0'), false); // false
+console.log(isValidIP('12.34.56'), false); // false*/
