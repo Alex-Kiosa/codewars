67e534231558cc8d8403e3516f4d1a1ca0523f0f
Alex-Kiosa <akiosa88@gmail.com>
2022-05-09 10:52:12 +0400

    wrote funcction arrayDiff

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 "use strict"
 
-let count = 0;
+function arrayDiff(a, b) {	
+	a.forEach(element => {
+		let index;
+		for(let i = 0; i < b.length; i++) {
+			index = a.indexOf(b[i]);
+			if (index > -1) {
+				a.splice(index, 1);
+			}
+		}
+	});
 
-function cc(card) {
-  // Only change code below this line
-
-  if (/[2-6]/.test(card)) {
-    count ++;
-  } else if (card === 10 || /[JQKA]/.test(card)) {
-  	count --;
-  }
-
-  if (count > 0) return count + " Bet";
-  return count + " Hold";
-  // Only change code above this line
+	return a;
 }
 
-console.log(cc('2'));
-console.log(cc('J'));
-console.log(cc('9'));
-console.log(cc('2'));
-console.log(cc('7'));
+console.log(arrayDiff([3,1,2], [1]), [3,2], "a was [3,1,2], b was [1]");
+console.log(arrayDiff([1,2,2], [2]), [1], "a was [1,2,2], b was [2]");
+console.log(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
+console.log(arrayDiff([1,2,2], []), [1,2,2], "a was [1,2,2], b was []");
+console.log(arrayDiff([1,2], [1]), [2], "a was [1,2], b was [1]");
+console.log(arrayDiff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1]");
+console.log(arrayDiff([], [1,2]), [], "a was [], b was [1,2]");
