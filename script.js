533d8022b59ae56791b8234930b7a85d26f81359
Alex <akiosa88@gmail.com>
2024-01-14 14:26:34 +0400

    init: init project

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function arrayDiff(a, b) {	
+	return a.filter(element => b.indexOf(element) == -1);
+}
+
+console.log(arrayDiff([3,1,2], [1]), [3,2], "a was [3,1,2], b was [1]");
+console.log(arrayDiff([1,2,2], [2]), [1], "a was [1,2,2], b was [2]");
+console.log(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
+console.log(arrayDiff([1,2,2], []), [1,2,2], "a was [1,2,2], b was []");
+console.log(arrayDiff([1,2], [1]), [2], "a was [1,2], b was [1]");
+console.log(arrayDiff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1]");
+console.log(arrayDiff([], [1,2]), [], "a was [], b was [1,2]");
