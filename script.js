b17c2630f3a8523353f7e48d3b1e1f2b4106cc84
Alex <akiosa88@gmail.com>
2024-01-15 00:32:02 +0400

    feat: write function firstNonRepeatingLetter()

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 "use strict"
 
-function arrayDiff(a, b) {	
-	return a.filter(element => b.indexOf(element) == -1);
+//====== 1 =======
+
+/*function arrayDiff(a, b) {
+    return a.filter(element => b.indexOf(element) == -1);
 }
 
-console.log(arrayDiff([3,1,2], [1]), [3,2], "a was [3,1,2], b was [1]");
-console.log(arrayDiff([1,2,2], [2]), [1], "a was [1,2,2], b was [2]");
-console.log(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
-console.log(arrayDiff([1,2,2], []), [1,2,2], "a was [1,2,2], b was []");
-console.log(arrayDiff([1,2], [1]), [2], "a was [1,2], b was [1]");
-console.log(arrayDiff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1]");
-console.log(arrayDiff([], [1,2]), [], "a was [], b was [1,2]");
+console.log(arrayDiff([3, 1, 2], [1]), [3, 2], "a was [3,1,2], b was [1]");*/
+
+
+
+/* ====== 2. First non-repeating character ======
+
+Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.*/
+
+function firstNonRepeatingLetter(s) {
+    for (let i = 0; i < s.length; i++) {
+        let re = new RegExp(s[i], "gi")
+
+        if (s.match(re).length === 1) return s[i]
+    }
+
+    return ''
+}
+console.log(firstNonRepeatingLetter('a'));
