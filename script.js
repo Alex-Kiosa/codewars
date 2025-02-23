5db7aa6bc2e4ba13311ea6e08598760623103843
Alex <akiosa88@gmail.com>
2025-02-23 23:10:33 +0400

    feat: wrote Simple Pig Latin function

--- script.js
+++ script.js
@@ -31,38 +31,26 @@ If a string contains all repeating characters, it should return an empty string 
 // console.log(firstNonRepeatingLetter('a'));
 
 
-/* ====== 3. First non-repeating character ======
+/* ====== 4 ======
 Given an array arr of strings, complete the function by calculating the total perimeter of all the islands. Each piece of land will be marked with 'X' while the water fields are represented as 'O'. Consider each tile being a perfect 1 x 1 piece of land. Some examples for better visualization:
  */
 
-/*
-function landPerimeter(arr) {
-    let horizontalRibs = 0
-    let verticalRibs = 0
+// function landPerimeter(arr) {
+//     // finding horizontalRibs
+//     for (let y = 0; y < arr.length; y++) {
+//         for (let x= 0; x< arr[i].length; x++) {
 
-    // finding horizontalRibs
-    for (let i = 0; i < arr[0].length; i++) {
-        if (arr[0][i] === "X") {
-            horizontalRibs = 2
-        }
+//         }
+//     }
 
-        if (arr[0][i] === "X" && arr[0][i-1] && arr[0][i-1] === "X") {
-            horizontalRibs++
-        }
+//     return `Total land perimeter: 60`
+// }
 
-        if (arr[0][i] === "X" && arr[0][i-1] && arr[0][i-1] === "O") {
-            continue
-        }
-    }
-
-    return `Total land perimeter: 60`
-}
-
-console.log(landPerimeter(["OXOOOX", "OXOXOO", "XXOOOX", "OXXXOO", "OOXOOX", "OXOOOO", "OOXOOX", "OOXOOO", "OXOOOO", "OXOOXX"])); // Total land perimeter: 60
-console.log(landPerimeter(["OXOOO", "OOXXX", "OXXOO", "XOOOO", "XOOOO", "XXXOO", "XOXOO", "OOOXO", "OXOOX", "XOOOO", "OOOXO"])); // Total land perimeter: 52*/
+// console.log(landPerimeter(["OXOOOX", "OXOXOO", "XXOOOX", "OXXXOO", "OOXOOX", "OXOOOO", "OOXOOX", "OOXOOO", "OXOOOO", "OXOOXX"])); // Total land perimeter: 60
+// console.log(landPerimeter(["OXOOO", "OOXXX", "OXXOO", "XOOOO", "XOOOO", "XXXOO", "XOXOO", "OOOXO", "OXOOX", "XOOOO", "OOOXO"])); // Total land perimeter: 52
 
 
-/* ====== 3. Get factorial ======
+/* ====== 5. Get factorial ======
 /*
 function getFactorial(n) {
     if (n === 0 || n === 1) { // базовый случай
@@ -75,7 +63,7 @@ function getFactorial(n) {
 console.log(getFactorial(98))*/
 
 
-/* ====== 3. Get factorial ======
+/* ====== 6. Recursive traversal ======
 let company = {
     sales: [{
         name: 'John',
@@ -119,3 +107,34 @@ function sumSalaries(department) {
 }
 
 console.log(sumSalaries(company))*/
+
+/* ====== 7. palindrome ======*/
+// function isPalindrome(string) {
+//     const str = string.toLowerCase()
+//
+//     return str.split('').reverse().join('') === str
+// }
+
+/* ====== 8. checkCoupon ====== */
+/*
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    return new Date(currentDate) <= new Date(expirationDate) && enteredCode === correctCode
+}*/
+
+// ====== 9. Simple Pig Latin ======
+function pigIt(str){
+    const arr = str.split(' ')
+    for (let i = 0; i < arr.length; i++) {
+        if(!/[^\w\s]/.test(arr[i])) {
+            arr[i] = arr[i].substring(1, arr[i].length) + arr[i][0] + 'ay'
+        }
+    }
+
+    // const arr = str.split(' ').map((element) => element = element.substring(1, element.length) + element[0] + 'ay')
+
+    return arr.join(' ')
+}
+
+console.log(pigIt('Pig latin is cool')) //igPay atinlay siay oolcay
+console.log(pigIt('This is my string')) //hisTay siay ymay tringsay
+console.log(pigIt('Hello world !')) //elloHay orldway !
